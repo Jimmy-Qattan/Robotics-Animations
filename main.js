fbcd1231307f684ceee1c3dcbf636b1f69a9ac8d
Jimmy-Qattan <59592522+Jimmy-Qattan@users.noreply.github.com>
2026-05-04 22:18:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ class ANIMATION {
     this.#defaultServoValue = 90;
 
     this.initialize();
+    this.checkIfPause();
   }
 
   static #animationParalysis = false;
@@ -45,8 +46,16 @@ class ANIMATION {
   static #ACTIVE = [];
   static #ACTIVECUT = [];
   static #currentServoPositions;
+  static #lastRoundedServoPosition;
+
+  static #centerPosition;
+
+  static #interpolationTypes = ["linear", "quadratic"];
 
   static servoPositionsInitialized = false;
+
+  static RUNNING = false;
+  static INTERUPTTED = false;
 
   static HELP(color = "green", color2 = "red", color3 = "yellow") {
     ANIMATION.printColor("INSTRUCTIONS FOR SETTING UP ANIMATION FRAME:", color);
@@ -134,6 +143,10 @@ class ANIMATION {
     return JSON.stringify(x) === JSON.stringify(y);
   }
 
+  static get getServoCount() {
+    return this.#ACTIVE[0].getNumOfServos;
+  }
+
   static arraysNoNegatives(x) {
     if (!(x instanceof Array)) return;
 
@@ -199,6 +212,14 @@ class ANIMATION {
     }
 
     return true;
+  }
+
+  checkIfPause() {
+    // LEFT OFF HERE in order to set pause when initielized
+    this.#isPause = ANIMATION.areArraysSame(
+      this.getInitialDestination,
+      this.getFinalDestination,
+    );
   }
 
   trimArrays(
@@ -358,7 +379,7 @@ class ANIMATION {
     this.setFinalDestination(arr2);
   }
 
-  setFinalDestination(arr) {
+  async setFinalDestination(arr, run = false) {
     // SETTER FOR FINAL SERVO POSITIONS
     if (!this.checkArray(arr)) {
       this.setFinalDestination(
@@ -369,16 +390,27 @@ class ANIMATION {
       );
       return;
     }
+
+    this.checkIfPause();
+
     if (ANIMATION.areArraysSame(arr, this.#initialServoPositions)) {
       this.#isPause = true;
 
       ANIMATION.printError(
         this.getName,
-        `ANIMATION HAS BEEN SET TO A PAUSE FOR " + ${this.#time} + " MILLISECONDS`,
+        `ANIMATION HAS BEEN SET TO A PAUSE FOR ${this.#time} MILLISECONDS`,
       );
       return;
     } else {
-      this.#finalServoPositions = arr;
+      if (run) {
+        await this.runToPosition(arr).then(() => {
+          this.#finalServoPositions = arr;
+          console.log(this.#finalServoPositions + " YAYAYYYYY");
+        });
+      } else {
+        this.#finalServoPositions = arr;
+      }
+
       this.#isPause = false;
     }
   }
@@ -391,15 +423,26 @@ class ANIMATION {
     return this.#finalServoPositions;
   }
 
-  setInitialDestination(arr) {
+  async setInitialDestination(arr, run = false) {
     // SETTER FOR INITIAL SERVO POSITONS
     if (!this.checkArray(arr)) {
       return;
     }
+
+    this.checkIfPause();
+
     // if (ANIMATION.arrayAllNums(arr) /**Checks values AND if array length is greater than 0 */) {
 
     // }
-    this.#initialServoPositions = arr;
+
+    if (run) {
+      await this.runToPosition(arr).then(() => {
+        this.#initialServoPositions = arr;
+        console.log(this.#initialServoPositions + " YAYAYYYYY");
+      });
+    } else {
+      this.#initialServoPositions = arr;
+    }
   }
 
   get getInitialDestination() {
@@ -450,6 +493,8 @@ class ANIMATION {
     // SETTER FOR NAME
     if (typeof name != "string" || String(name).length < 1) return;
     this.#name = String(name);
+
+    ANIMATION.printColor(`Name Successfully Set To ${name}`);
   }
 
   get getName() {
@@ -570,16 +615,19 @@ class ANIMATION {
     return this.#isPause;
   }
 
+  static get getCurrentServoPositions() {
+    return this.#currentServoPositions;
+  }
+
   get determineStepSize() {
     if (!this.initialized) return;
 
-    let currentSpeeds = ANIMATION.initArray(this.getNumOfServos);
-    currentSpeeds?.forEach((value, index) => {
-      currentSpeeds[index] = value / this.getTime;
+    const stepSizes = this.getFinalDestination.map((element, index) => {
+      return (element - this.getInitialDestination[index]) / this.getTime;
     });
 
-    this.setStepSizes(currentSpeeds);
-    return currentSpeeds;
+    this.setStepSizes(stepSizes);
+    return stepSizes;
   }
 
   setStepSizes(arr) {
@@ -682,7 +730,7 @@ class ANIMATION {
     // Append new animationFrame's to ANIMATION.#ACTIVECUT
     if (!ANIMATION.servoPositionsInitialized) {
       ANIMATION.printColor(
-        "Servo Positions Must be Initialized before running ANIMATION.initializeActiveAnimationServoCounts()",
+        "Servo Positions Must be Initialized before running ANIMATION.initializeActiveAnimationServoCounts() using initializeServoPositions([servo position array])",
         "red",
       );
       return;
@@ -708,11 +756,16 @@ class ANIMATION {
     });
   }
 
-  static isReady() {
+  static degreesToRadians(deg) {
+    return (deg * Math.PI) / 180;
+  }
+
+  static get isReady() {
     if (
-      ANIMATION.#animationParalysis ||
-      !ANIMATION.#animationsAllowed ||
-      !ANIMATION.#ACTIVECUT.length
+      this.#animationParalysis ||
+      !this.#animationsAllowed ||
+      !this.#ACTIVE.length ||
+      this.RUNNING
     )
       return false;
 
@@ -720,11 +773,434 @@ class ANIMATION {
     return true;
   }
 
-  async runAnimation() {
-    const movePromise = new Promise((resolve, reject) => {});
+  addStep() {
+    const newPosArray = ANIMATION.#currentServoPositions.map(
+      (element, index) => {
+        return element + this.#stepSizes[index];
+      },
+    );
+
+    return newPosArray;
+  }
+
+  static INTERUPT() {
+    if (this.RUNNING) {
+      this.INTERUPTTED = true;
+      this.printColor("ANIMATION HAS BEEN INTERUPPTED");
+    } else {
+      this.printColor(
+        "No Animations are Running, therefore No Interupptions Were Made",
+      );
+    }
+  }
+
+  checkIfAbsoluteValueOfAnimationArrayChanged() {
+    const newRoundedValues = ANIMATION.roundAllValuesInArray(
+      ANIMATION.getCurrentServoPositions,
+      true,
+    );
+    const newRoundedValues2 = ANIMATION.roundAllValuesInArray(
+      this.addStepToCurrentServoPositionsValue,
+      true,
+    );
+
+    if (!ANIMATION.areArraysSame(newRoundedValues, newRoundedValues2)) {
+    }
+  }
+
+  addStepToCurrentServoPositions() {
+    //if (!ANIMATION.isReady || !this.initialized) return;
+    //ANIMATION.#currentServoPositions += this.determineStepSize; OMFG
+
+    ANIMATION.getCurrentServoPositions.forEach((value, index) => {
+      ANIMATION.#currentServoPositions[index] +=
+        this.determineStepSize[index] * 100;
+    });
+
+    return ANIMATION.getCurrentServoPositions;
+  }
+
+  get addStepToCurrentServoPositionsValue() {
+    //if (!ANIMATION.isReady || !this.initialized) return;
+
+    const newArray = ANIMATION.getCurrentServoPositions.map((value, index) => {
+      return value + this.determineStepSize[index];
+    });
+
+    return newArray;
+  }
+
+  static roundAllValuesInArray(arr, strict = false) {
+    const newArray = arr.map((value) => {
+      return Math.round(strict ? value - 0.5 : value);
+    });
+
+    return newArray;
+  }
+
+  static get lastRoundedServoValues() {
+    return this.#lastRoundedServoPosition;
+  }
+
+  static #checkNewRoundedValuesForServoTransit(newArray) {
+    if (
+      !this.areArraysSame(
+        this.#lastRoundedServoPosition,
+        this.roundAllValuesInArray(newArray, true),
+      )
+    ) {
+      window.dispatchEvent(
+        new CustomEvent("animationData", {
+          detail: this.#lastRoundedServoPosition,
+        }),
+      );
+      this.#setRoundedServoValuesFromUnRounded(newArray);
+      console.log(this.lastRoundedServoValues);
+      setRotationsOf3DFiguresFromDegrees(this.#lastRoundedServoPosition);
+    }
+  }
+
+  static #setRoundedServoValuesFromUnRounded(arr) {
+    this.#lastRoundedServoPosition = this.roundAllValuesInArray(arr, true);
+  }
+
+  async runAnimation(fromCurrentServoPositions = false) {
+    if (!ANIMATION.isReady) {
+      return;
+    }
+
+    if (ANIMATION.INTERUPTTED) {
+      return;
+    }
+
+    if (
+      ANIMATION.areArraysSame(
+        ANIMATION.getCurrentServoPositions,
+        this.getFinalDestination,
+      )
+    ) {
+      ANIMATION.printColor("Final Destination has already been reached");
+      return;
+    }
+
+    ANIMATION.RUNNING = true;
+
+    const beginningDestinationOriginal = this.getInitialDestination;
+
+    const currentServoPositions = ANIMATION.getCurrentServoPositions;
+    this.setInitialDestination(currentServoPositions);
+
+    const thisAnimation = this;
+
+    const animationDuration = this.getTime;
+
+    let animationId;
+
+    let startTime, now;
+    startTime = now = performance.now();
+
+    return new Promise((resolve, reject) => {
+      animationId = requestAnimationFrame(run);
+
+      function run() {
+        now = performance.now();
+
+        const interpolation = (now - startTime) / animationDuration;
+
+        if (interpolation >= 1 && !ANIMATION.INTERUPTTED) {
+          if (
+            !ANIMATION.areArraysSame(
+              ANIMATION.getCurrentServoPositions,
+              thisAnimation.getFinalDestination,
+            )
+          ) {
+            thisAnimation.setInitialAndFinalDest(
+              ANIMATION.roundAllValuesInArray(beginningDestinationOriginal),
+              ANIMATION.roundAllValuesInArray(
+                thisAnimation.getFinalDestination,
+              ),
+            );
+
+            ANIMATION.#currentServoPositions =
+              thisAnimation.getFinalDestination;
+
+            ANIMATION.#lastRoundedServoPosition =
+              ANIMATION.getCurrentServoPositions;
+          }
+
+          resolve("Success");
+          ANIMATION.printColor(
+            "Animation Successful! Stats: " +
+              ANIMATION.getCurrentServoPositions,
+            "green",
+          );
+
+          ANIMATION.RUNNING = false;
+          cancelAnimationFrame(animationId);
+          return;
+        }
+
+        if (ANIMATION.INTERUPTTED) {
+          reject("Interupption");
+          cancelAnimationFrame(animationId);
+          return;
+        }
+
+        // thisAnimation.checkIfAbsoluteValueOfAnimationArrayChanged(
+        //   ANIMATION.getCurrentServoPositions,
+        // );
+        // thisAnimation.checkIfAbsoluteValueOfAnimationArrayChanged();
+
+        //thisAnimation.addStepToCurrentServoPositions();
+        // Instead of having an addStep Function, we can just create a function that determines position based on time interpolation
+        // It would be much more straightforth, cleaner, more accurate, and easier to manipulate when it comes to other interpolation types that are non-linear
+
+        const newPositionFromInterpolation =
+          thisAnimation.determinePositionFromInterpolation(
+            interpolation,
+            fromCurrentServoPositions
+              ? ANIMATION.getCurrentServoPositions
+              : thisAnimation.getInitialDestination,
+            thisAnimation.getFinalDestination,
+          );
+
+        ANIMATION.#checkNewRoundedValuesForServoTransit(
+          newPositionFromInterpolation,
+        );
+        ANIMATION.#setRoundedServoValuesFromUnRounded(
+          newPositionFromInterpolation,
+        );
+
+        setRotationsOf3DFiguresFromDegrees(ANIMATION.#lastRoundedServoPosition);
+
+        //console.log(ANIMATION.#lastRoundedServoPosition);
+
+        //console.log(ANIMATION.getCurrentServoPositions);
+
+        animationId = requestAnimationFrame(run);
+      }
+    });
+  }
+
+  determinePositionFromInterpolation(
+    interpolation,
+    initial,
+    final,
+    interpolationThreshold = 0.005,
+  ) {
+    // To pass in interpolation, then initial position, then final position
+
+    if (1 - interpolation <= interpolationThreshold) {
+      return final;
+    }
+
+    let initialPosition = initial;
+    let finalPosition = final;
+
+    let differenceArray = finalPosition.map((value, index) => {
+      return value - initialPosition[index];
+    });
+
+    let modifiedInterpolationConstant;
+
+    switch (this.getInterpolationType) {
+      case "linear":
+        modifiedInterpolationConstant = interpolation;
+        break;
+      case "quadratic":
+        modifiedInterpolationConstant = Math.pow(interpolation, 2);
+        break;
+      default:
+        console.log(
+          "There is a bug within the interpolation initialization. There should only be 2 options right now",
+        );
+    }
+
+    const returnArray = initialPosition.map((value, index) => {
+      return value + modifiedInterpolationConstant * differenceArray[index];
+    });
+    return returnArray;
+
+    console.log(returnArray);
+  }
+
+  static setTimeAll(arr, time, fillerTimes = 5000) {
+    if (arr instanceof ANIMATION) {
+      if (time instanceof Array) {
+        arr.setTime(time[0]);
+      } else if (Number.isFinite(time)) {
+        arr.setTime(time);
+      } else {
+        this.printColor(
+          "Value in first parameter must be a singular Animation (best done using instancename.setTime(time)) or an array of ANIMATION Objects",
+        );
+        return;
+      }
+
+      this.printColor(
+        "Changing one instance can be done just using instanceName.setTime(time)",
+      );
+
+      return;
+    }
+
+    if (!(arr instanceof Array))
+      return new Error(
+        "Instances must be an array of Animation Objects. Changing One Instance should be done using instance.setTime(time)",
+      );
+
+    arr.forEach((animation, index) => {
+      if (!(animation instanceof ANIMATION)) {
+        this.printColor(
+          `This Instance of ${typeof animation} + Must Be An Animation Object`,
+        );
+      }
+
+      if (Number.isFinite(time)) {
+        animation.setTime(time);
+      } else if (time instanceof Array) {
+        animation.setTime(time[index] ?? time[time.length - 1]);
+      } else {
+        return new Error(
+          "Time Must be either a single number of an array of numbers",
+        );
+      }
+    });
+  }
+
+  static numRepeat(num) {
+    return this.initArray(this.#ACTIVE[0].getNumOfServos ?? 0, num);
+  }
+
+  setFinalPositionRandomized(center = 90, range = 90, modifyOriginal = false) {
+    if (center < 0 || center > 180 || !Number.isFinite(center)) return;
+    if (!Number.isFinite(range)) return;
+    let initialArray = ANIMATION.initArray(this.getNumOfServos, center);
+
+    const finalArray = initialArray.map((value) => {
+      const randomNumber =
+        Math.floor(Math.random() * Math.abs(range)) - Math.abs(range) / 2;
+
+      let modifiedValue = value + randomNumber;
+
+      if (modifiedValue < 0) {
+        modifiedValue = 0;
+      } else if (modifiedValue > 180) {
+        modifiedValue = 180;
+      }
+
+      return Math.ceil(modifiedValue);
+    });
+
+    if (modifyOriginal) {
+      this.setFinalDestination(finalArray);
+    }
+
+    return finalArray;
+  }
+
+  runToCenterPosition() {
+    if (!ANIMATION.#centerPosition) return;
+    const currentServoPositionFinal = this.getFinalDestination;
+    this.setFinalDestination(ANIMATION.#centerPosition);
+    this.runAnimation().then(() => {
+      this.setFinalDestination(currentServoPositionFinal);
+      ANIMATION.printColor(
+        `Animation Final Destination set back to ${currentServoPositionFinal}`,
+        "green",
+      );
+    });
+  }
+
+  async runToPosition(position) {
+    if (!this.checkArray(position)) {
+      this.runAnimation();
+      return;
+    }
+
+    const currentDestinationFinal = this.getFinalDestination;
+    const currentDestinationInitial = this.getInitialDestination;
+
+    this.setFinalDestination(position);
+
+    console.log(`Going from ${currentDestinationInitial} to ${position}`);
+
+    await this.runAnimation().then(() => {
+      this.setInitialAndFinalDest(
+        currentDestinationInitial,
+        currentDestinationFinal,
+      );
+    });
+  }
+
+  async runToRandomPosition(center = 90, range = 90, modifyOriginal = false) {
+    if (
+      !ANIMATION.isReady ||
+      !Number.isFinite(center) ||
+      !Number.isFinite(range)
+    )
+      return;
+
+    const currentFinalDestination = !modifyOriginal
+      ? this.getFinalDestination
+      : null;
+    const currentInitialDestination = !modifyOriginal
+      ? this.getInitialDestination
+      : null;
+
+    const currentInterpolationType = !modifyOriginal
+      ? this.getInterpolationType
+      : null;
+
+    this.setFinalPositionRandomized(center, range, true);
+
+    this.setInterpolationType(
+      ANIMATION.#interpolationTypes[
+        Math.floor(Math.random() * (ANIMATION.#interpolationTypes.length - 1))
+      ],
+    );
+
+    ANIMATION.printColor(
+      `Interpolation type set to ${this.getInterpolationType}`,
+    );
+
+    await this.runAnimation().then(() => {
+      if (!modifyOriginal) {
+        console.log(currentFinalDestination);
+        this.setInitialAndFinalDest(
+          currentInitialDestination,
+          currentFinalDestination,
+        );
+        this.setInterpolationType(currentInterpolationType);
+        ANIMATION.printColor(
+          `Final Destination points set back to ${currentFinalDestination} and interpolation type is set back to ${currentInterpolationType}`,
+        );
+      }
+    });
   }
 }
 
-// Have a function that moves to a certain position in a certain time
 // Use the time to determine an interpolation for each individual servo and user can choose the type of interpolation
 // Have animationParalysis utilized in order to wait until the bird is finished with it's motion before doing anything else
+
+const k = new ANIMATION();
+const j = new ANIMATION();
+k.setInitialAndFinalDest([0, 0, 0, 0], [180, 180, 180, 180]);
+k.pushToActive();
+j.pushToActive();
+ANIMATION.initializeServoPositions([90, 90, 90, 90]);
+
+ANIMATION.setTimeAll([k, j], 3000);
+
+function setRotationsOf3DFiguresFromDegrees(rotations) {
+  const allRotatableFigures = document.querySelectorAll("[rotation-listener]");
+
+  const newDegMapping = rotations.map((value) => {
+    return ANIMATION.degreesToRadians(value) - Math.PI / 2;
+  });
+
+  allRotatableFigures.forEach((figure, index) => {
+    const axisOfRot = figure.getAttribute("rotation-listener").axis;
+    figure.object3D.rotation[axisOfRot] = newDegMapping[index];
+  });
+}
